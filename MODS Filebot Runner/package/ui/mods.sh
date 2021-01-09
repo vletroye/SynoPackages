cd625cc8a241dc1dcb35335207d802314a56ce8c
vletroye <vletroye@gmail.com>
2021-01-09 14:12:45 +0100

    New Package to run a filebot task

---
+++ MODS Filebot Runner/package/ui/mods.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+stdbuf -oL echo "Executing FileBot Task 1 as" $(whoami)
+stdbuf -oL echo "___________________________________________________________________"
+stdbuf -oL echo
+stdbuf -oL echo "Please Wait. FileBot needs some time to start."
+stdbuf -oL echo
+stdbuf -oL echo
+
+stdbuf -oL /usr/local/filebot-node/task 1
+
+stdbuf -oL echo
+stdbuf -oL echo "_____________________________________________"
