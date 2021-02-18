6290dd2663c3ce21cb0a758914fa6981e090eee9
vletroye <vletroye@gmail.com>
2021-02-18 17:44:21 +0100

    New sample to illustrate the deployment with sudoer account of a package using a CGI router on DSM 7.x

---
+++ MODS Sample - Advanced CGI 7.x/package/ui/content/script.php
@@ -0,0 +1,6 @@
+<p>
+<button type="button" onclick="loadDoc('content/simple_ajax.txt')">Simple Ajax</button><br/><br/>
+<button type="button" onclick="loadDocCallBack('content/callback_ajax.txt', myFunction)">Ajax with CallBack</button>
+</p>
+<hr/>
+<div id="demo">...</div>
