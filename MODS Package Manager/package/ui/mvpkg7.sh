0d875c7227c4b5068b57bcb99c356c4ee91bacc4
Valéry Letroye <vletroye@gmail.com>
2022-10-11 21:43:15 +0200

    Create mvpkg7.sh
    This script can be used to move simple packages on DSM 7.x
    ./mv.sh <target volume> <package name>

---
+++ MODS Package Manager/package/ui/mvpkg7.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+TARGET=$1
+PACKAGE=$2
+
+if [[ $PACKAGE == "" ]]
+then
+	echo "Usage: mvpkg Target Package"
+	echo "       Target must be like 'volumex' or 'volumeUSBx' where x is a numeric."
+	echo "       Package must be the name of a package."
+	exit
+fi
+
+if [[ $TARGET != volume?(USB)[0-9]* ]]
+then
+	echo "Usage: mvpkg Target Package"
+	echo "       Target must be like 'volumex' or 'volumeUSBx' where x is a numeric."
+	echo "       Package [$PACKAGE] must be the name of a package."
+	exit
+fi
+
+#Check the package and check the result: "enable" (is start), "disable" (is stop) or "does not exist"
+output=$(synopkg status "$PACKAGE")
+
+if [[ $output == *"non_installed"* ]]
+then
+	echo "The service $PACKAGE can't be found."
+	exit
+else
+	#find the current volume of the package and its link
+	output=$( ls -la /var/packages/*/target | grep "/$PACKAGE/")
+	
+	link=$(echo $output | grep -oP "\/var/packages/.*/target")
+	volume=$(echo $output | grep -oP "volume(USB)?\d*")
+	path=$(echo $output | grep -oP "\/volume(USB)?.*")
+	
+	if [[ $link != "/var/packages/$PACKAGE/target"* ]]
+	then
+		echo "The service $PACKAGE is not correctly installed."
+		exit
+	fi
+	
+	if [[ $volume != "volume"* ]]
+	then
+		echo "The service $PACKAGE can't be located."
+		exit
+	fi
+
+	if [[ $volume == $TARGET ]]
+	then
+		echo "The service $PACKAGE is already on $TARGET."
+		exit
+	fi
+	
+	if [[ "$path" != "/$volume/@appstore/$PACKAGE" ]]
+	then
+		echo "The service $PACKAGE does not have a standard location."
+		exit
+	fi
+		
+	#List Packages with dependency on this one
+	#synopkg --reverse-dependency $PACKAGE
+			
+	#Stop the package and all its dependencies
+	output=$(synopkg stop "$PACKAGE")
+	
+	if [[ $output != *"stopped"* ]]
+	then
+		echo "The service $PACKAGE couldn't be stopped."
+		exit
+	fi	 
+  
+	if [ -d "/$TARGET/@appstore/$PACKAGE" ]; then
+		mv "/$TARGET/@appstore/$PACKAGE" "/$TARGET/@appstore/$PACKAGE-$(date -d "today" +"%Y%m%d%H%M").log"
+	fi
+
+	#remove the link on the previous volume
+	rm -f "$link"
+	
+	#move the package
+	mv "$path" /$TARGET/@appstore
+	
+	#link with the  package on the new volume
+	ln -s "/$TARGET/@appstore/$PACKAGE" "$link"
+	
+	#Replace link also in local 
+	local="/usr/local/$PACKAGE"
+	if [ -L "$local" ]; then
+		rm -f "$local"
+		ln -s "/$TARGET/@appstore/$PACKAGE" "$local"
+	fi
+
+	#Replace link also in 3rdparty if pointing at @appstore
+	trdparty="/usr/syno/synoman/webman/3rdparty/$PACKAGE"
+	if [ -L "$trdparty" ]; then
+		#check the link of the 3rdparty
+		output=$( ls -la $trdparty | grep "/$volume/@appstore")
+
+		if [[ ! -z "output" ]]
+		then
+			rm -f "$trdparty"
+			ln -s "/$TARGET/@appstore/$PACKAGE" "$trdparty"
+		fi
+	fi
+			
+	#update settings
+	sed -i "s/$volume/$TARGET/" "/usr/syno/etc/packages/$PACKAGE/*" &>/dev/null
+	
+	#Restart packages depending on the one moved
+	#output=$(/usr/syno/sbin/synoservicecfg --reverse-dependency "pkgctl-$PACKAGE")
+
+	#output="$(echo $output | grep -Po "pkgctl-([^\]]*)")"
+	#for string in $output
+	#do
+	#	synopkg start "$string"
+	#done	
+
+	#Restart the package and all its dependencies
+	#output=$(/usr/syno/sbin/synoservicecfg --hard-start "pkgctl-$PACKAGE" | grep Service)
+  output=$(synopkg start "$PACKAGE")
+	
+	#Check if the package has been correctly restarted
+	#output=$(/usr/syno/sbin/synoservicecfg --is-enabled "pkgctl-$PACKAGE")
+
+	if [[ $output != *"started"* ]]
+	then
+		echo "The service $PACKAGE didn't restart properly once moved from $volume to $TARGET."
+	else	
+		echo "The service $PACKAGE has been moved successfuly from $volume to $TARGET."
+	fi
+fi
